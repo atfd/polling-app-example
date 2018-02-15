beed4d7b00de148eb84f5d78afd6506ff6e025b6
John Korzhuk <jkorzhuk@gmail.com>
2018-02-15 13:09:02 -0500

    Added styled-components - Worked on bost post

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}>
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+const HeaderContainer = styled.header`
+  ${props => props.backgroundColor};
+  margin-bottom: 1.45rem;
+`;
+
+const HeaderWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+const Heading1 = styled.h1`
+  margin: 0;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const BACKGROUND_COLOR =
+  'background-image: linear-gradient(116deg, #08AEEA 0%, #2AF598 100%)';
+
+const Header = ({ backgroundColor }) => (
+  <HeaderContainer backgroundColor={backgroundColor}>
+    <HeaderWrapper>
+      <Heading1>
+        <StyledLink to="/">Polling App</StyledLink>
+      </Heading1>
+    </HeaderWrapper>
+  </HeaderContainer>
 );
+
+Header.defaultProps = {
+  backgroundColor: BACKGROUND_COLOR,
+};
+
+Header.propTypes = {
+  backgroundColor: PropTypes.string,
+};
 
 export default Header;
