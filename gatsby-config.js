0bfa9490aac2e20b227280165386b51fa76d862d
John Korzhuk <jkorzhuk@gmail.com>
2018-04-02 15:35:21 -0400

    added voting functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,12 @@ module.exports = {
   siteMetadata: {
     title: 'Polling App',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/poll/*`] },
+    },
+  ],
 };
---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const config = {
+// apiKey: ,
+// authDomain: ,
+// databaseURL: ,
+// projectId: ,
+// storageBucket: ,
+// messagingSenderId: ,
+};
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(config);
+    this.store = firebase.firestore;
+    this.auth = firebase.auth;
+  }
+
+  get polls() {
+    return this.store().collection('polls');
+  }
+}
+
+export default new Firebase();
